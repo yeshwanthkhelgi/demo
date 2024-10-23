fd8308886e3fa466d30baa94a38cbe27d539e00c
yeshwanthkhelgi <123920318+yeshwanthkhelgi@users.noreply.github.com>
2024-10-23 11:26:25 +0530

    Create script.rb

---
+++ CICDScripts/script.rb
@@ -0,0 +1,13 @@
+# script.rb
+
+# Access environment variables
+env_var1 = ENV['ENV_VAR1']
+env_var2 = ENV['ENV_VAR2']
+custom_env_var1 = ENV['CUSTOM_ENV_VAR1']
+custom_env_var2 = ENV['CUSTOM_ENV_VAR2']
+
+# Output the values
+puts "ENV_VAR1: #{env_var1}"
+puts "ENV_VAR2: #{env_var2}"
+puts "CUSTOM_ENV_VAR1: #{custom_env_var1}"
+puts "CUSTOM_ENV_VAR2: #{custom_env_var2}"
