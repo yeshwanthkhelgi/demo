70a8a11aa5d33ebc270ed80207da17e026d6a205
yeshwanthkhelgi <123920318+yeshwanthkhelgi@users.noreply.github.com>
2025-02-20 16:51:26 +0530

    Update script.rb

--- CICDScripts/script.rb
+++ CICDScripts/script.rb
@@ -13,3 +13,19 @@ puts "CUSTOM_ENV_VAR1: #{custom_env_var1}"
 puts "CUSTOM_ENV_VAR2: #{custom_env_var2}"
 
 #hello
+def release_lead_check
+  code_owner_content = File.read(".github/CODEOWNERS")
+  code_owner_content = code_owner_content.match(/^\* (.*)$/)
+  if code_owner_match
+    release_leads = code_owner_match[1].gsub('@',' ').split(' ')
+    if release_leads.empty?
+      raise "Erros: No release leads"
+    else
+      puts "found release leads : #{release_leads.join(', ')}"
+    end
+  else
+    raise "Erros: No release leads"
+  end
+end
+
+release_lead_check
