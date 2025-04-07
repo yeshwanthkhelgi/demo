35a205212a9c493432d4d2e04aba438a9d138d58
yeshwanthkhelgi <123920318+yeshwanthkhelgi@users.noreply.github.com>
2025-04-07 09:59:10 +0530

    Update script.rb

--- CICDScripts/script.rb
+++ CICDScripts/script.rb
@@ -13,7 +13,10 @@ puts "CUSTOM_ENV_VAR1: #{custom_env_var1}"
 puts "CUSTOM_ENV_VAR2: #{custom_env_var2}"
 
 #hello
-File.open(ENV['GITHUB_ENV'], 'a') do |file|
-  file.puts("link=some_link")
-end
+# File.open(ENV['GITHUB_ENV'], 'a') do |file|
+#   file.puts("link=some_link")
+# end
+File.open(ENV['GITHUB_ENV'], 'a') { |f| f.puts("link=https://example.com/generated_link") }
+File.write('link.txt', 'https://example.com/generated_link')
+
 
